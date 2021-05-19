48c566611c7dbb85e08922c98d64537c2836bda7
Alisson Moraes <alissonmoraesdev@gmail.com>
2021-05-19 10:57:38 -0300

    Desafio Launchbase [ Pagina de cursos dinamica ]

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "MRQvG7Pq7dY",
+    title: "Já ouviu falar do CSS focus-within?",
+    duration: "4 min",
+    price: "Free"
+  },
+  {
+    id: "NpTF7HsuG5U",
+    title: "Como fazer commits atômicos pelo VSCode",
+    duration: "6 min",
+    price: "Free"
+  },
+  {
+    id: "G1a0qYwQZ2Q",
+    title: "COMO FAZER O DEPLOY DE UM PROJETO FRONT END (SUPER RÁPIDO!) (iniciantes)",
+    duration: "2 min",
+    price: "Free"
+  },
+  {
+    id: "kJM5bFjmMXE",
+    title: "Como lidar com as frustrações na carreira Dev?",
+    duration: "19 min",
+    price: "Free"
+  },
+  {
+    id: "ruoHSuTKp-U",
+    title: "Desestruturação no javascript com exemplos - Destructuring Assignment JS",
+    duration: "16 min",
+    price: "Free"
+  },
+  {
+    id: "WYtFMrLeBo8",
+    title: "Técnica de estudo pra programar - Active Recall",
+    duration: "8 min",
+    price: "Free"
+  },  
+]
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const server = express();
+const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false,
 })
 
 server.get('/', function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars.githubusercontent.com/u/7413619?v=4",
+    name: "Alisson Moraes",
+    role: "Desenvolvedor Fullstack",
+    description: "Criando soluções para os problemas do dia a dia nesse mundão.",
+    links: [
+      { name: "Github", url: "https://github.com/AlissonMoraesDev"},
+      { name: "Instagram", url: "https://www.instagram.com/alissonmoraesdev/"},
+      { name: "LinkedIn", url: "https://www.linkedin.com/in/alissonmoraesdev/"}
+    ]
+  }
+  return res.render("about", { about })
 })
 
 server.get('/portfolio', function(req, res) {
-  return res.render("portfolio")
+  return res.render("portfolio", { items: videos })
+})
+
+server.get('/cursos', function(req, res) {
+  const curso = {
+    modulo: [
+      { name: "Lógica de programação", time_estimed: "2 semanas"},
+      { name: "Desenvolvimento web fundamentos", time_estimed: "4 semanas"},
+      { name: "Deploy da Aplicação", time_estimed: "1 semana"}
+    ]
+  }
+
+  return res.render("cursos", { curso })
 })
 
 server.use(function(req, res) {
